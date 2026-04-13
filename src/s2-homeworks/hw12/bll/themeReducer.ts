e1765d0b363261fb324e4ba8bcea8a3b00d1f768
Dima Berry <dimaberry1@gmail.com>
2026-04-13 15:16:41 +0300

    feat: complete hw-12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,23 @@
 const initState = {
-    themeId: 1,
-}
+  themeId: 1,
+};
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export type StateType = {
+  themeId: number;
+};
 
-        default:
-            return state
-    }
-}
+export const themeReducer = (state = initState, action: ChangeThemeIdActionType): StateType => {
+  switch (action.type) {
+    case "SET_THEME_ID":
+      return { ...state, themeId: action.id };
+    default:
+      return state;
+  }
+};
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ChangeThemeIdActionType = {
+  type: "SET_THEME_ID";
+  id: number;
+};
+
+export const changeThemeId = (id: number): ChangeThemeIdActionType => ({ type: "SET_THEME_ID", id });
